834b305fb73d51d40e29c3ed752b9d4167643ac6
brajenful <zolee12@indamail.hu>
2017-09-08 07:37:37 +0200

    added config file support, username support, general bugfixes

--- twitch-arduino/twitch-arduino.py
+++ twitch-arduino/twitch-arduino.py
@@ -1,5 +1,6 @@
 import serial, time
 import config
+import requests
 from twitch import TwitchClient
 CLIENT_ID = config.CLIENT_ID
 OAUTH_ID = config.OAUTH_ID
@@ -11,7 +12,6 @@ INTERVAL = config.INTERVAL
 user_id = None
 client = TwitchClient(CLIENT_ID, OAUTH_ID)
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=TIMEOUT)
-follows_displayname = []           #display names of followed channels
 follows_channelname = []           #channel names of followed channels
 follows_userids = []               #user IDs of followed channels
 streams_online = []                #user IDs of live channels
@@ -25,22 +25,22 @@ def login():
 		user_id = user.id
 
 def getfollows(): #gets followed channels and puts them into a list
-	del follows_displayname[:]
+	global follows_channelname
 	del follows_channelname[:]
 	e = client.users.get_follows(user_id)
-	for i in range(len(e)): #gets the display name
-		follows_displayname.insert(i, e[i]['channel']['display_name'])
 	for i in range(len(e)): #gets the channel name
 		follows_channelname.insert(i, e[i]['channel']['name'])
 
 def getids(): #gets followed channels' user IDs and puts them into a list
+	global follows_userids
 	del follows_userids[:]
-	for i in range(len(follows_displayname)):
+	for i in range(len(follows_channelname)):
 		users = client.users.translate_usernames_to_ids(follows_channelname[i])
 		for user in users:
 			follows_userids.insert(i, user.id)
 
 def update_online(): #takes every channel that is online from the follows_userids list and puts them into the streams_online list
+	global streams_online
 	del streams_online[:]
 	for i in range(len(follows_userids)):
 		e = client.streams.get_stream_by_user(follows_userids[i])
@@ -49,17 +49,20 @@ def update_online(): #takes every channel that is online from the follows_userid
 
 def update_new(): #checks for any new live channels, and if there's any, sends a notification through the serial port and puts them in the streams_old list
 	print('Checking for streams...')
+	global streams_new
 	streams_new = streams_online
-	while streams_new:
-		if streams_new[0] not in streams_old:
-			e = client.streams.get_stream_by_user(streams_new[0])
-			time.sleep(2)
-			ser.write(b'%r' % (e['channel']['display_name']))
-			print('%s is live!' % (e['channel']['display_name']))
-			streams_old.insert(0, streams_new[0])
-			del streams_new[0]
+	if streams_new[0]:
+		for i in range(len(streams_new)):
+			if streams_new[0] not in streams_old:
+				e = client.streams.get_stream_by_user(streams_new[0])
+				time.sleep(2)
+				ser.write(b'%r' % (e['channel']['display_name']))
+				print('%s is live!' % (e['channel']['display_name']))
+				streams_old.insert(0, streams_new[0])
+				del streams_new[0]
 
 def update_old(): #checks if the channels are still online, if not, removes them from the list
+	global streams_old
 	if streams_old:
 		for i in range(len(streams_old)):
 			e = client.streams.get_stream_by_user(streams_old[i])
@@ -73,11 +76,14 @@ def main(e):
 	login();
 	print('Login successful!')
 	while True:
-		getfollows();
-		getids();
-		update_online();
-		update_new();
-		update_old();
+		try:
+			getfollows();
+			getids();
+			update_online();
+			update_new();
+			update_old();
+		except requests.exceptions.HTTPError:
+			print('Internal server error, trying again...')
 		time.sleep(e)
 
 if __name__ == '__main__':
